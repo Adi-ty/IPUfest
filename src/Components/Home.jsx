ff28f47d6abb9dbe896237ff09993ae35534b026
Aditya <adityasinghboss1234@gmail.com>
2023-02-03 19:49:22 +0530

    scroll effect

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,80 +1,70 @@
-import React from 'react';
-import { motion, useCycle } from 'framer-motion';
-import './Home.scss';
+import React, { useEffect } from 'react';
+import './home.scss';
 
 const Home = () => {
-  const [index, cycleIndex] = useCycle(0, 1, 2, 3);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollY = window.scrollY;
+      const heroSection = document.querySelector('.hero');
+      heroSection.style.backgroundPositionY = `-${scrollY * 0.4}px`;
+
+      const heroLeft = document.querySelector('.hero-left');
+      heroLeft.style.backgroundPositionY = `${scrollY * 0.6}px`;
+
+      const events = document.querySelector('.events');
+      events.style.transform = `translateY(${scrollY * 0.7}px)`;
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
   return (
     <div className="home">
       <div className="hero">
         <div className="hero-left">
-          <motion.div
-            animate={{ x: [0, -100, 0, 100, 0] }}
-            transition={{ duration: 4, ease: 'easeInOut', loop: Infinity }}
-            className="hero-left-light"
-          />
+          <div className="hero-left-light"></div>
         </div>
         <div className="hero-right">
           <div className="hero-right-content">
-            <img src="logo.png" alt="Event logo" />
-            <h1>The Best Tech Event</h1>
-            <p>Join us for an unforgettable experience.</p>
+            <img src="./logo.png" alt="Event Logo" />
+            <h1>The Best Event</h1>
+            <p>The best event of the year is here!</p>
           </div>
         </div>
       </div>
       <div className="sponsors">
         <h2>Our Sponsors</h2>
         <div className="sponsors-list">
-          <img src="sponsor1.png" alt="Sponsor 1" />
-          <img src="sponsor2.png" alt="Sponsor 2" />
-          <img src="sponsor3.png" alt="Sponsor 3" />
+          <img src="./sponsor1.png" alt="Sponsor 1" />
+          <img src="./sponsor2.png" alt="Sponsor 2" />
+          <img src="./sponsor3.png" alt="Sponsor 3" />
         </div>
       </div>
       <div className="events">
-        <h2>Featured Events</h2>
-        <motion.div className="events-carousel">
-          <motion.div
-            animate={{ x: [-100 * index, -100 * cycleIndex] }}
-            transition={{ duration: 0.5 }}
-            className="event-card"
-          >
-            <img src="event1.jpg" alt="Event 1" />
+        <h2>Our Upcoming Events</h2>
+        <div className="events-carousel">
+          <div className="event-card">
+            <img src="./event1.png" alt="Event 1" />
             <h3>Event 1</h3>
-            <p>Description of Event 1</p>
-          </motion.div>
-          <motion.div
-            animate={{ x: [-100 * index, -100 * cycleIndex] }}
-            transition={{ duration: 0.5 }}
-            className="event-card"
-          >
-            <img src="event2.jpg" alt="Event 2" />
+          </div>
+          <div className="event-card">
+            <img src="./event2.png" alt="Event 2" />
             <h3>Event 2</h3>
-            <p>Description of Event 2</p>
-          </motion.div>
-          <motion.div
-            animate={{ x: [-100 * index, -100 * cycleIndex] }}
-            transition={{ duration: 0.5 }}
-            className="event-card"
-          >
-            <img src="event3.jpg" alt="Event 3" />
+          </div>
+          <div className="event-card">
+            <img src="./event3.png" alt="Event 3" />
             <h3>Event 3</h3>
-            <p>Description of Event 3</p>
-          </motion.div>
-          <motion.div
-            animate={{ x: [-100 * index, -100 * cycleIndex] }}
-            transition={{ duration: 0.5 }}
-            className="event-card"
-          >
-            <img src="event4.jpg" alt="Event 4" />
-            <h3>Event 4</h3>
-            <p>Description of Event 4</p>
-          </motion.div>
-        </motion.div>
-      </div>
+          </div>
+        </div>
+        
       <footer>
         <p>&copy; Developer by SDC</p>
       </footer>
+      </div>
     </div>
   );
 };
