b713cffb6dd8621d0b0a4ba1b66142df1e3d063d
Aditya <adityasinghboss1234@gmail.com>
2023-02-05 09:49:30 +0530

    all pages init

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,28 @@
-import './App.css'
-import Home from './Components/Home'
-import Navbar from './Components/Navbar'
+import React from 'react'
+import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import Events from './Components/Events/Events'
+import Footer from './Components/Footer/Footer'
+import Hackathon from './Components/Hackathon/Hackathon'
+import Navbar from './Components/Header/Navbar'
+import Home from './Components/Home/Home'
+import Team from './Components/Team/Team'
+import Contact from './Components/ContactUs/Contact'
+import PageNotFound from './Components/404/PageNotFound'
 
 function App() {
-  
   return (
-    <div>
+    <Router>
       <Navbar />
-      <Home />
-    </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path='/events' element={<Events />} />
+        <Route path='/hackathon' element={<Hackathon />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ src/Components/404/PageNotFound.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function PageNotFound() {
+  return (
+    <div>The developer of this website is very lazy he might have forgot to build this page for you.</div>
+  )
+}
+
+export default PageNotFound
